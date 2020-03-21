9e1ddfa87bd57c8487325c52510bb199de000a84
VinczeRobert <vinczerobert7@gmail.com>
2020-03-21 19:42:43 +0200

    COMMIT #5: Initial training model. We will keep changing this one until we get a good enough accuracy. So far it is somewhere around 50%, which is good for starting.

--- cnn_architecture/cnn_architecture.py
+++ cnn_architecture/cnn_architecture.py
@@ -1,21 +1,41 @@
 from keras import models, layers, losses
 
+from hyper_parameters.hyper_parameters import NUMBER_OF_FILTERS_1, NUMBER_OF_FILTERS_2, NUMBER_OF_FILTERS_3, \
+    NUMBER_OF_FILTERS_4, KERNEL_SIZE, STRIDES, ACTIVATION_FUNCTION, POOL_SIZE, INPUT_SHAPE, DROPOUT_RATE, \
+    DENSE_UNITS, NUMBER_OF_CLASSES, SECOND_ACTIVATION_FUNCTION
 
-class CNNArchitecture():
+
+class CNNArchitecture:
 
     def __init__(self):
         self.model = models.Sequential()
 
     def build_model(self):
-        self.model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
-        self.model.add(layers.MaxPooling2D((2, 2)))
-        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-        self.model.add(layers.MaxPooling2D((2, 2)))
-        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_1, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION,
+                                     input_shape=INPUT_SHAPE))
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_1, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.MaxPooling2D(pool_size=POOL_SIZE))
 
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_2, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_2, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.MaxPooling2D(pool_size=POOL_SIZE))
+
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_3, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.Conv2D(filters=NUMBER_OF_FILTERS_4, kernel_size=KERNEL_SIZE,
+                                     strides=STRIDES, padding="same", activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.MaxPooling2D(pool_size=POOL_SIZE))
+
+        self.model.add(layers.BatchNormalization())
         self.model.add(layers.Flatten())
-        self.model.add(layers.Dense(64, activation='relu'))
-        self.model.add(layers.Dense(10))
+        self.model.add(layers.Dropout(rate=DROPOUT_RATE))
+
+        self.model.add(layers.Dense(units=DENSE_UNITS, activation=ACTIVATION_FUNCTION))
+        self.model.add(layers.Dense(units=NUMBER_OF_CLASSES, activation=SECOND_ACTIVATION_FUNCTION))
 
         print(self.model.summary())
 
