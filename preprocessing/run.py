81cac033262d3ebbb8d379755e5aa16859cc1222
VinczeRobert <vinczerobert7@gmail.com>
2020-04-20 14:04:13 +0300

    COMMIT #13: Added run.py to image_preprocessing directory, which can be used for different purposes. Currently I am using it to initiate conversion to binary of an entire folder.

---
+++ preprocessing/run.py
@@ -0,0 +1,5 @@
+from preprocessing.image_preprocessing import convert_folder_to_binary
+
+if __name__ == '__main__':
+    path = 'C:/Users/robi997/Downloads/Hand_Gesture_Dataset/Testing/Only_Letters'
+    convert_folder_to_binary(path)
