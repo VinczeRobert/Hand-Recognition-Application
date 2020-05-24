f5d9019e740fc35e324f1d604c14cab5491fa92d
VinczeRobert <vinczerobert7@gmail.com>
2020-05-24 15:35:47 +0300

    COMMIT #36: Included these in this branch as well

---
+++ model/frame_captor.py
@@ -0,0 +1,52 @@
+import requests
+import cv2 as cv
+import numpy as np
+from base_constants.general_constants import CAPTURING_MODE, HAND
+from requests.exceptions import ConnectionError, Timeout
+
+class FrameCaptor:
+
+    def __init__(self, init_url=''):
+        """
+        This class is used to continuosly capture frames from the camera in real time and to prepare and to edit them
+        with display messages and the rectangle representing the area of interest, where users are required to put their hand signs
+        :param url: url to Android IPWebcam server; if it is a valid one, the application will use the Android phone's camera
+        :param hand: 0 for right hand, 1 for left hand
+        """
+        self._init_url = init_url + '/shot.jpg'
+        self._capturing_mode = None
+        self._camera = None
+
+    def set_capture_mode(self):
+        # if the url is not empty and is valid then the Android camera will be used
+        if self._init_url != '/shot.jpg':
+            try:
+                requests.get(self._init_url, timeout=5)
+                self._capturing_mode = 1
+                return
+            except (ConnectionError, Timeout) as e:
+                print(e)
+                print('Invalid URL or the server is taking too much time. The built-in camera will be used instead')
+
+        # if the url is not valid, the PC camera will be used by default
+        self._capturing_mode = 0
+        self._camera = cv.VideoCapture(0)
+        self._camera.set(10, 200)
+        self._camera.set(3, 1280)
+
+    def read_frame(self):
+        """
+        Reads frame from camera input, applies bilateral filter on it and display responses to the user
+        :param is_background_captured: boolean parameter to check if the background has been extracted
+        :param predicted_letter: the response of the CNNArchitecture for the currently shown hand sign
+        :return: frame with messages
+        """
+        if CAPTURING_MODE[self._capturing_mode] == 'PC_CAMERA':
+            _, frame = self._camera.read()
+        else:
+            image_response = requests.get(self._init_url)
+            image_array = np.array(bytearray(image_response.content), dtype=np.uint8)
+            frame = cv.imdecode(image_array, -1)
+            frame = cv.resize(frame, (1280, 720))
+
+        return frame
---
+++ utils/create_dataset.py
@@ -0,0 +1,74 @@
+import os
+import cv2 as cv
+import timeit
+from model.frame_captor import FrameCaptor
+from model.frame_displayer import FrameDisplayer
+from model.image_preprocessor import ImagePreprocessor
+
+
+# TODO: Add functionality to append the images to the existing ones, not override them
+def create_data_for_class(path_to_folder, class_name, is_binary=False, start_count=1, end_count=1000,
+                          with_cropping=False, camera_init_url='', hand_index=0, var_threshold=50):
+    # Creating folders if they are not existing
+    if not os.path.exists(path_to_folder):
+        os.mkdir(path_to_folder)
+
+    image_path = os.path.join(path_to_folder, class_name)
+
+    if not os.path.exists(image_path):
+        os.mkdir(image_path)
+
+    # Initializing
+    frame_captor = FrameCaptor(camera_init_url)
+    frame_captor.set_capture_mode()
+    image_preprocessor = ImagePreprocessor(hand_index, var_threshold)
+    frame_display = FrameDisplayer(hand_index)
+    start = False
+    background_captured = False
+
+    while True:
+        image = frame_captor.read_frame()
+        start_time = timeit.default_timer()
+        flipped_image = cv.flip(image, 1)
+
+        if background_captured is True:
+            preprocessed_image = image_preprocessor.prepare_image_for_classification(flipped_image,
+                                                                                         is_binary=is_binary,
+                                                                                         with_cropping=with_cropping)
+
+            if start:
+                save_path = os.path.join(image_path, class_name + '_{}.jpg'.format(start_count))
+
+                while True:
+                    stop_time = timeit.default_timer()
+                    difference = stop_time - start_time
+
+                    if difference > 0.25:
+                        cv.imwrite(save_path, preprocessed_image)
+                        start_count = start_count + 1
+                        break
+
+            if start_count > end_count:
+                break
+
+        frame_display.display_frame(flipped_image, start_count)
+
+        key = cv.waitKey(10)
+
+        if key == ord('s') and background_captured:
+            start = True
+        elif key == ord('b'):
+            background_captured = True
+            image_preprocessor.set_background_subtractor()
+        elif key == ord('q'):
+            cv.destroyAllWindows()
+            exit(0)
+
+    cv.destroyAllWindows()
+    exit(0)
+
+
+if __name__ == '__main__':
+    path_to_folder = 'D:/Hand Gesture Datasets/Self_Made_ASL_Dataset'
+    class_name = 'Z'
+    create_data_for_class(path_to_folder, class_name, hand_index=1, start_count=139)
